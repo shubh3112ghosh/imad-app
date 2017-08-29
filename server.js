5d8840cecfc0481a15ff63b8f1bb7a4fffbd6d26
shubh3112ghosh <31139676+shubh3112ghosh@users.noreply.github.com>
2017-08-29 15:04:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('this is the article-one');
+  res.send('this is the article-one');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile('this is the article-two');
+  res.send('this is the article-two');
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile('this is the article-three');
+  res.send('this is the article-three');
 });
 
 
